c3bb59b955c932e5a42348918ef763feb98821ab
Karima-Aktari <karimaaktari911@gmail.com>
2023-08-10 12:37:36 +0600

    class 5-6

---
+++ class6.php
@@ -0,0 +1,21 @@
+<?php
+
+$girl = 'Sophia Rahman';
+
+if ($girl === 'Sophia' || $girl === 'Sophia Rahman'){
+    echo 'Sophia is a beautiful girl.';
+} else {
+    echo 'Sophia is my heart';
+}
+
+
+echo '<br/>';
+
+
+$girl = 'Sophia Rahman';
+
+if ($girl === 'Sophia' && $girl === 'Sophia Rahman'){
+    echo 'Sophia is a beautiful girl.';
+} else {
+    echo 'Sophia is my heart';
+}
